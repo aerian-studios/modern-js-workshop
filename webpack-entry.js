b9c47a9ac3822a20689e5bf54bb54d5ff6d0735a
Matt Kane <ascorbic@gmail.com>
2018-03-16 11:36:23 +0000

    Add webpack-only entry point

--- webpack-entry.js
+++ webpack-entry.js
@@ -1,2 +1,6 @@
+/**
+ * This file is to allow us to import polyfill node modules, which won't work untranspiled.
+ * This lets webpack load those modules, and then load index.js as usual.
+ */
 import "isomorphic-unfetch";
 import "./index";
