496bbd09397ad0cf46c3ea254fb998fa459f4df4
Matt Kane <ascorbic@gmail.com>
2018-03-16 11:47:13 +0000

    We use polyfill.io for this, rather than bundling

--- webpack-entry.js
+++ webpack-entry.js
@@ -2,5 +2,4 @@
  * This file is to allow us to import polyfill node modules, which won't work untranspiled.
  * This lets webpack load those modules, and then load index.js as usual.
  */
-import "isomorphic-unfetch";
 import "./index";
