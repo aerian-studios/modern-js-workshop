07c06fafa72f866f863ba9085f16be476098f6d7
Sunya Dickman <sunya.dickman@aerian.com>
2018-04-12 17:48:15 +0100

    Prepared master for week 09

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
     entry: "./webpack-entry.js",
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.[jt]sx?$/,
                 exclude: /node_modules/,
                 use: "ts-loader",
             },
@@ -18,4 +19,16 @@ module.exports = {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
     },
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: __dirname,
+        compress: true,
+        port: 5000,
+        hot: true,
+        inline: true,
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
 };
