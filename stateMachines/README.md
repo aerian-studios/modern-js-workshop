2a09af9e5d46a4a871f0d96fd83cae9f7c266a66
sunya <sunya.dickman@aerian.com>
2018-08-10 14:02:23 +0100

    tweKA

--- stateMachines/README.md
+++ stateMachines/README.md
@@ -1,1 +1,22 @@
 # State Machines - the rise of the robot ...er goverment :robot:
+
+## What are State Machines and like... Why?
+
+State Machines are a way of declaring and formalising the states that something
+can exist in and how and when it can transition from one state to another. They
+specifically define the relationship between different states and importantly,
+don't allow any other relationship to occur. This makes them really good for
+some things and not so good for others. They would be very useful for describing
+the on/off state of a checkbox, or the angle of the hands of a clock for example
+because they have a finite set of states and a finite set of inputs (ways that
+something can change). On the other hand, they are bad at describing the the
+position of something that hasn't a fixed set of positions (think cars on a
+track or people at a festival vs tick-tack-to or chess).
+
+Why? There are 3 good reasons to use them:
+
+1. They are a very compact way to describe the states of something.
+2. They are easy to design and reason about (when they aren't too complex) - you
+   can even generate pretty pictures to help you
+3. They are highly automatable/testable because they have fixed states, inputs
+   and transitions
--- stateMachines/src/App.js
+++ stateMachines/src/App.js
@@ -2,88 +2,127 @@ import React from "react";
 import { Action, State, withStateMachine } from "react-automata";
 import LoginForm from "./components/LoginForm";
 import { MessageOverlay } from "./components/MessageOverlay";
-import { loginFlow } from "./loginFlow";
+// import { loginFlow } from "./loginFlow";
+
+const loginFlow = {
+    initial: "LoggedOut",
+    states: {
+        LoggedOut: {
+            onEntry: "showLoginForm",
+            on: {
+                LOGIN: "LoggingIn",
+            },
+        },
+        LoggingIn: {
+            onEntry: "showLoggingInMessage",
+            on: {
+                ERROR: "Error",
+                SUCCESS: "Success",
+            },
+        },
+        Success: {
+            onEntry: "showSuccessMessage",
+            on: {
+                TIMEOUT: "LoggedIn",
+            },
+        },
+        Error: {
+            onEntry: "showErrorMessage",
+            on: {
+                TIMEOUT: "LoggedOut",
+            },
+        },
+        LoggedIn: {
+            onEntry: "showLoggedIn",
+            on: {
+                LOGOUT: "LoggedOut",
+            },
+        },
+    },
+};
 
 const mockUserDetails = {
-  username: "aerian",
-  password: "isCool,reallyCool"
+    username: "aerian",
+    password: "isCool,reallyCool",
 };
 
 const messages = {
-  error: "There was an error with your username or password",
-  success: "You have been logged in",
-  info: "Logging in..."
+    error: "There was an error with your username or password",
+    success: "You have been logged in",
+    info: "Logging in...",
 };
 
 const DEFAULT_STATE = {
-  message: ""
+    message: "",
 };
 
 export class App extends React.Component {
-  state = DEFAULT_STATE;
+    state = DEFAULT_STATE;
 
-  mockLoginAPI = async (username, password) => {
-    await window.setTimeout(() => {
-      if (
-        username !== mockUserDetails.username ||
-        password !== mockUserDetails.password
-      ) {
-        this.props.transition("ERROR");
-      } else {
-        this.props.transition("SUCCESS");
-      }
-    }, 5000);
-  };
+    mockLoginAPI = async (username, password) => {
+        await window.setTimeout(() => {
+            if (
+                username !== mockUserDetails.username ||
+                password !== mockUserDetails.password
+            ) {
+                this.props.transition("ERROR");
+            } else {
+                this.props.transition("SUCCESS");
+            }
+        }, 5000);
+    };
 
-  showLoggingInMessage() {
-    this.setState({ message: "info" });
-  }
+    showLoggingInMessage() {
+        this.setState({ message: "info" });
+    }
 
-  showErrorMessage() {
-    this.setState({ message: "error" });
-  }
+    showErrorMessage() {
+        this.setState({ message: "error" });
+    }
 
-  showSuccessMessage() {
-    this.setState({ message: "success" });
-  }
+    showSuccessMessage() {
+        this.setState({ message: "success" });
+    }
 
-  componentWillTransition(event) {
-    console.log({ event });
-  }
+    componentWillTransition(event) {
+        console.log({ event });
+    }
 
-  handleMessageClose = () => {
-    this.props.transition("TIMEOUT");
-  };
+    handleMessageClose = () => {
+        this.props.transition("TIMEOUT");
+    };
 
-  handleClick = ({ username, password }) => {
-    this.props.transition("LOGIN");
-    this.mockLoginAPI(username, password);
-  };
+    handleClick = ({ username, password }) => {
+        this.props.transition("LOGIN");
+        this.mockLoginAPI(username, password);
+    };
 
-  render() {
-    return (
-      <main>
-        <p>{this.props.machineState && this.props.machineState.value}</p>
-        <State is={["LoggedOut", "LoggingIn", "Error", "Success"]}>
-          <LoginForm login={this.handleClick} />
-        </State>
-        <Action is="showLoggingInMessage">
-          <MessageOverlay messageType={this.state.message}>
-            {messages[this.state.message]}
-          </MessageOverlay>
-        </Action>
-        <Action is={["showErrorMessage", "showSuccessMessage"]}>
-          <MessageOverlay
-            onClosed={this.handleMessageClose}
-            timeout={true}
-            messageType={this.state.message}
-          >
-            {messages[this.state.message]}
-          </MessageOverlay>
-        </Action>
-      </main>
-    );
-  }
+    render() {
+        return (
+            <main>
+                <p>
+                    {this.props.machineState && this.props.machineState.value}
+                </p>
+                <State is={["LoggedOut", "LoggingIn", "Error", "Success"]}>
+                    <LoginForm login={this.handleClick} />
+                </State>
+                <Action is="showLoggingInMessage">
+                    <MessageOverlay messageType={this.state.message}>
+                        {messages[this.state.message]}
+                    </MessageOverlay>
+                </Action>
+                <Action is={["showErrorMessage", "showSuccessMessage"]}>
+                    <MessageOverlay
+                        onClosed={this.handleMessageClose}
+                        timeout={true}
+                        messageType={this.state.message}
+                    >
+                        {messages[this.state.message]}
+                    </MessageOverlay>
+                </Action>
+            </main>
+        );
+    }
 }
 
 export default withStateMachine(loginFlow)(App);
