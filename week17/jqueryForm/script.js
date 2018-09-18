b2aaf05f69b74bdbf22a3e204b61c9a5a35928cc
Darrem Ivey <darren.ivey@aerian.com>
2018-09-18 09:14:32 +0100

    Provide user feedback to form submision

--- week17/jqueryForm/script.js
+++ week17/jqueryForm/script.js
@@ -5,16 +5,25 @@ $( document ).ready(function() {
         var userName = event.target.username.value;
             email = event.target.email.value,
             password1 = event.target.password1.value,
-            password2 = event.target.password2.value,
+            password2 = event.target.password2.value;
+        var formCompleted = userName && email && password1 && password2;
 
-        console.table([
-            ["userName", userName], 
-            ["email:", email], 
-            ["password1:", password1], 
-            ["password2:", password2]
-        ])
+        if (formCompleted && (password1 === password2)) {
+            // Make post here, if end point existed
+            // $.post(url);
 
-        // Make post here is end point existed
-        // $.post(url);
+            $(".footer__error").removeClass("footer__error--display");
+            $(".footer__button").html("Submited &#10003;");
+
+            console.table([
+                ["user name", userName], 
+                ["email:", email], 
+                ["password1:", password1], 
+                ["password2:", password2]
+            ])
+        } else {
+            $(".footer__error").addClass("footer__error--display");
+            $(".footer__button").html("Try again");        
+        }
     })
 });
