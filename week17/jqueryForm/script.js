b04f2c75ba3910f1801a000ea03353967588dfcd
Darrem Ivey <darren.ivey@aerian.com>
2018-09-18 21:05:59 +0100

    Refactor

--- week17/jqueryForm/script.js
+++ week17/jqueryForm/script.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
     
-    function handlePasswordMatch(event) {
+    function handlePasswordCheck() {
         var password1 = $('#password1').val();
         var password2 = $('#password2').val();
         var passwordMatch = password1 === password2;
@@ -9,34 +9,43 @@ $( document ).ready(function() {
         $(".footer__error").toggleClass("footer__error--display", !passwordMatch);
     }
 
+    function handleSubmition () {
+        $(".footer__error").removeClass("footer__error--display");
+        $(".footer__button").html("Submited &#10003;");
+    }
+
+    function handleRetry () {
+        $(".footer__button").html("Try again");      
+    }
+
+    function logResults (userName, email, password1, password2) {
+        console.table([
+            ["user name", userName], 
+            ["email:", email], 
+            ["password1:", password1], 
+            ["password2:", password2]
+        ])
+    }
+
     function submitForm(event) {
+        event.preventDefault();
         var userName = event.target.username.value;
             email = event.target.email.value,
             password1 = event.target.password1.value,
             password2 = event.target.password2.value;
+        var formCompleted = userName && email && password1 && password2;
 
-        event.preventDefault();
-
-        if ((userName && email && password1 && password2) && (password1 === password2)) {
-            // Make post here, if end point existed
-            // $.post(url);
-
-            $(".footer__error").removeClass("footer__error--display");
-            $(".footer__button").html("Submited &#10003;");
-
-            console.table([
-                ["user name", userName], 
-                ["email:", email], 
-                ["password1:", password1], 
-                ["password2:", password2]
-            ])
+        if (formCompleted && (password1 === password2)) {
+            // Post form data
+            handleSubmition();
+            logResults(userName, email, password1, password2);
         } else {
-            $(".footer__button").html("Try again");        
+            handleRetry();
         }
     }
 
     $("#password2").keyup(function (event) {
-        handlePasswordMatch(event);
+        handlePasswordCheck(event);
     })
 
     $("#form").submit(function (event) {
