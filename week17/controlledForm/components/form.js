ea48bf9980cf3ba8a3eb1e2d1e2ea50d1732678b
Darrem Ivey <darren.ivey@aerian.com>
2018-09-19 21:40:31 +0100

    Format

--- week17/controlledForm/components/form.js
+++ week17/controlledForm/components/form.js
@@ -34,10 +34,22 @@ class Form extends Component {
                 }}>
                     <h1 className="form__header">Would you like to know more?</h1>
                     <fieldset className="form_fieldset">
-                        <FormField filedText="Username" filedId="username" type="text" callBack={ (e, id) => { this.gatherFieldData(e, id) }} />
-                        <FormField filedText="Email" filedId="email" type="text" callBack={ (e, id) => { this.gatherFieldData(e, id) }} />
-                        <FormField filedText="Password" filedId="password1" type="text" callBack={ (e, id) => { this.gatherFieldData(e, id) }} />
-                        <FormField filedText="Please confirm password" filedId="password2" type="text" callBack={ (e, id) => { this.gatherFieldData(e, id) }} />
+                        <FormField filedText="Username"
+                                   filedId="username"
+                                   type="text"
+                                   callBack={ (e, id) => { this.gatherFieldData(e, id) }} />
+                        <FormField filedText="Email"
+                                   filedId="email"
+                                   type="text"
+                                   callBack={ (e, id) => { this.gatherFieldData(e, id) }} />
+                        <FormField filedText="Password"
+                                   filedId="password1"
+                                   type="text"
+                                   callBack={ (e, id) => { this.gatherFieldData(e, id) }} />
+                        <FormField filedText="Please confirm password"
+                                   filedId="password2"
+                                   type="text"
+                                   callBack={ (e, id) => { this.gatherFieldData(e, id) }} />
                     </fieldset>
                     <Footer />
                 </form>
